37b07a2ab3a8c3187005209673306a9d14635a5a
Sayan Sadhukhan <77845946+sayansadhukhan538@users.noreply.github.com>
2024-03-04 15:35:18 +0530

    first commit

---
+++ server/config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+exports.connect = () => {
+  mongoose
+    .connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB connected."))
+    .catch((error) => console.log("There is an error in DB connection", error));
+};
